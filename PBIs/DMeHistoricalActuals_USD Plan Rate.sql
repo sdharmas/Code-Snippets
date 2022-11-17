d2e69fa6148ca184fbc361f5dccc2977f40a144d
sdharmas <102329855+sdharmas@users.noreply.github.com>
2022-11-17 09:29:31 -0800

    Add QRF Current

--- PBIs/DMeHistoricalActuals_USD Plan Rate.sql
+++ PBIs/DMeHistoricalActuals_USD Plan Rate.sql
@@ -361,3 +361,75 @@ group by [Scenario]
        , internal_offering_custom2
        , internal_offering_custom3
        , internal_offering_custom4
+union all
+-- Temp only to pull QRF Current
+-- QRF for Current Quarter, pull only 'USD Current Plan Rate'
+select [Scenario]
+     , [Route to Market - Subscription]
+     , [FX Conversion]
+     , [Market Segment]
+     , reg.[Region - Name]
+     , Current_FiscalWeek
+     , [App Names]
+     , [Ind vs Team vs Ent]
+     , [Offerings]
+     , [Enterprise BU - Name]
+     , [Fiscal Week]
+     , [LoadDate]
+     , [Measures2]
+     , [Measures3]
+     , [Measures4]
+     , [Offerings2]
+     , [Product1]
+     , [RoutetoMarket2]
+     , [Type2]
+     , [Fiscal Quarter]
+     , arr.[Geo]
+     , [Current Quarter]
+     , [FiscalWeekNum]
+     , internal_offering_custom0
+     , internal_offering_custom1
+     , internal_offering_custom2
+     , internal_offering_custom3
+     , internal_offering_custom4
+     , sum([Value]) as ValueAdj
+from [FINANCE_SYSTEMS].[dbo].[vw_TM1_Subs_Dash]                                     arr
+    inner join [FINANCE_SYSTEMS].[dbo].[TM1_Revenue_PlanningConsolidatedMarketArea] reg
+        on reg.[Market Area - Name] = arr.[Market Area - Name]
+where ([Fiscal Quarter] > [Current Quarter])
+      and [Scenario] = ('QRF - Current')
+      and
+      (
+          Measures3 = 'Beginning'
+          or Measures3 = 'Net New'
+      )
+      and [FX Conversion] = 'USD Current Plan Rate'
+      and [Type2] = 'Total ARR'
+group by [Scenario]
+       , [Route to Market - Subscription]
+       , [FX Conversion]
+       , [Market Segment]
+       , reg.[Region - Name]
+       , Current_FiscalWeek
+       , [App Names]
+       , [Ind vs Team vs Ent]
+       , [Offerings]
+       , [Enterprise BU - Name]
+       , [Fiscal Week]
+       , [LoadDate]
+       , [Measures2]
+       , [Measures3]
+       , [Measures4]
+       , [Offerings2]
+       , [Product1]
+       , [RoutetoMarket2]
+       , [Type2]
+       , [Fiscal Quarter]
+       , arr.[Geo]
+       , [Current Quarter]
+       , [FiscalWeekNum]
+       , internal_offering_custom0
+       , internal_offering_custom1
+       , internal_offering_custom2
+       , internal_offering_custom3
+       , internal_offering_custom4       
