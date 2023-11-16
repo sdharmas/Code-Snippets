99480808148bdba643cb910c520dd1d1f90f0e67
sdharmas <102329855+sdharmas@users.noreply.github.com>
2023-11-15 23:05:36 -0800

    Updated with QTD/QTG and Relative Day ( hardened)

--- PBIs/Trended Historical/ARR Trended.sql
+++ PBIs/Trended Historical/ARR Trended.sql
@@ -1,10 +1,12 @@
--- Remember to change the QRF scenario at the bottom, and also Plan (do we need it?)
+-- Remember to change variables before the dotted line section
 
 declare @CQtr varchar(10) = '2023 Q4'
-declare @CY varchar(10) = '2023'
 declare @QRFsnap varchar(50) = 'Dashboard QRF'
 declare @QRF varchar(50) = 'Q4 QRF 2023'
 declare @Plan varchar(50) = '2023 Plan'
+
+------------------------------------------------------------------------------------------
+declare @CY varchar(10) = left(@CQtr, 4)
 declare @lastdayofQtr varchar(50) =
         (
             select distinct
@@ -12,19 +14,16 @@ declare @lastdayofQtr varchar(50) =
             from [dbo].[Date_FiscalDateLookup]
             where [TM1_Fiscal Qtr/Year] = @CQtr
         )
-
 declare @today varchar(50) =
         (
             select format(getdate(), 'yyyy-MM-dd')
         )
-
 declare @CFQtoday varchar(50) =
         (
             select [TM1_Fiscal Qtr/Year]
             from Date_FiscalDateLookup
             where DayOfCalendarDate = @today
         )
-
 declare @RFQtoday varchar(50) =
         (
             select [Relative Fiscal Quarter]
@@ -41,6 +40,20 @@ declare @RFQ varchar(50) =
         (
             select case when @CFQtoday > @CQtr then @RFQQuarterEnd else @RFQtoday end
         )
+declare @RdayQuarterEnd varchar(50) =
+        (
+            select [Relative Day]
+            from Date_FiscalDateLookup
+            where DayOfCalendarDate = @lastdayofQtr
+        )
+declare @Rday varchar(50) =
+        (
+            select case when @today > @lastdayofQtr then @RdayQuarterEnd else 0 end
+        )
+-- select @today          today
+--      , @lastdayofQtr   lastday
+--      , @RdayQuarterEnd RdayQE
+--      , @Rday           Rday
 -- select distinct
 --        [Fiscal Quarter]
 --      , [Fiscal Week]
@@ -51,34 +64,12 @@ select @CQtr                            as 'Current Quarter'
      , [Scenario]
      , [Fiscal Quarter]
      , case
-           when (Measures3 = 'Beginning')
-                or
-                (
-                    Measures3 = 'Net New'
-                    and
-                    (
-                        (
-                            Scenario = 'Actuals - Segmentation'
-                            and FW.[Relative Fiscal Quarter] = @RFQ - 4
-                        )
-                        or
-                        (
-                            Scenario = 'Actuals'
-                            and FW.[Relative Fiscal Quarter] = @RFQ - 1
-                        )
-                        or
-                        -- The below is needed for the R/B/U Ending ARR Adjustment, we need to get the week grain
-                        (
-                            Scenario = 'Actuals'
-                            and [Fiscal Quarter] = '2023 Q1'
-                        )
-                    )
-                    and CAST(right(FW.TM1_Fiscal_WeekinQtr, 2) as int) <= AFW.ActualsFiscalWeek_int
-                ) then
-               [Fiscal Week]
+           when FiscalWeekNum < CW.WkNum
+                and [Route to Market - Subscription] <> 'Enterprise' then
+               'QTD'
            else
-               MaxFiscalWeekinYr
-       end                              as [Fiscal Week]
+               'QTG'
+       end                              as [QTD/QTG]
      , [Market Segment]
      , Offerings
      , [App Names]
@@ -117,126 +108,76 @@ select @CQtr                            as 'Current Quarter'
 from [FINANCE_SYSTEMS].[dbo].[vw_TM1_Subs_Dash] A
     left join
     (
-        select right(min([Fiscal WeekinYear]), 2)         as MinWeekinQtr
-             , right(max([Fiscal WeekinQtr]), 2)          as MaxWkinQtr
-             , replace([Fiscal Qtr/Year], '-', ' ')       as FiscalYrQtr
-             , max(replace([Fiscal WeekinYear], '-', '')) as MaxFiscalWeekinQtr_52
+        select right(TM1_Fiscal_WeekinQtr, 2) WkNum
         from [dbo].[Date_FiscalDateLookup]
-        group by replace([Fiscal Qtr/Year], '-', ' ')
-    )                                           Dt
-        on A.[Fiscal Quarter] = Dt.FiscalYrQtr
-    left join
-    (
-        select TM1_Fiscal_WeekinQtr
-             , [Fiscal WeekinYear]
-             , [Relative Fiscal Quarter]
-             , [Relative Fiscal Week]
-             , [Relative Fiscal Year]
-        from FINANCE_SYSTEMS.[dbo].[Date_FiscalDateLookup]
-        group by TM1_Fiscal_WeekinQtr
-               , [Fiscal WeekinYear]
-               , [Relative Fiscal Quarter]
-               , [Relative Fiscal Week]
-               , [Relative Fiscal Year]
-    )                                           FW
-        on replace(FW.[Fiscal WeekinYear], '-', '') = A.[Fiscal Week]
-    left join
-    (
-        select CAST(right(TM1_Fiscal_WeekinQtr, 2) as int) as ActualsFiscalWeek_int
-        from FINANCE_SYSTEMS.[dbo].[Date_FiscalDateLookup]
-        where [Relative Fiscal Week] = -1
-        group by CAST(right(TM1_Fiscal_WeekinQtr, 2) as int)
-    )                                           AFW
+        where [Relative Day] = @Rday
+    )                                           CW
         on 1 = 1
-    left join
-    (
-        select [Fiscal Year]                              as FY_Y
-             , max(replace([Fiscal WeekinYear], '-', '')) as MaxFiscalWeekinYr
-        from [dbo].[Date_FiscalDateLookup]
-        group by [Fiscal Year]
-    )                                           Yr
-        on A.[Fiscal Year] = Yr.FY_Y
 where --[Fiscal Quarter] = '2023 Q3' and
-     (
-          (
-              [Fiscal Year] >= '2016'
-              and [Fiscal Year] <= '2018'
-              and [Scenario] = 'Actuals'
-              and [Type2] = 'Total ARR'
-          ) -- Actuals Total ARR from 2016, 2017, 2018
-          or
-          (
-              [Fiscal Year] >= '2017'
-              and [Fiscal Year] <= '2018'
-              and [Scenario] = 'Actuals'
-              and [Type2] = 'Total Units'
-          ) -- Actuals Total Units from 2017, 2018
-          or
-          (
-              [Fiscal Year] >= '2019'
-              and [Fiscal Year] <= '2022'
-              and [Scenario] = ('Actuals - Segmentation')
-          )
-          or
-          (
-              [Scenario] = ('Actuals')
-              and [Relative Fiscal Year] = 0
-              and [Relative Fiscal Week] < 0
-          ) -- All Fiscal Weeks data in Current Year and less than Current Week 
-          or
-          (
-              [Fiscal Quarter] = @CQtr
-              and [Scenario] in ('Outlook - Current', 'OL - Previous')
-          ) --- Outlook - Current from Current Fiscal Quarter
-          or
-          (
-              [Fiscal Year] = @CY
-              and [Scenario] in ( @Plan, @QRF, @QRFsnap, 'Q2 QRF 2023', 'Q3 QRF 2023' )
-          )
-          or
-          (
-              [Fiscal Year] > @CY
-              and [Scenario] like ('%Strat Plan%')
-          )
-          or
-          (
-              [Fiscal Year] > @CY
-              and [Scenario] like ('%Ops Plan%')
-          )
-          or
-          (
-              [Fiscal Year] > @CY
-              and [Scenario] like ('%FY24 Plan%')
-          )
-          or
-          (
-              [Fiscal Year] > @CY
-              and [Scenario] in ('Dashboard QRF', '2024 Plan')
-          )          
-      )
-      and
-      (
-          (
-              Measures3 = 'Beginning'
-              and right([Fiscal Week], 2) = MinWeekinQtr
-          ) -- Get only data from first week of the quarter
-          or (Measures3 = 'Net New')
-      )
-      and 1 = case
-                  when [Type2] in ( 'Total ARR' )
-                       and [FX Conversion] in ( 'USD Current Plan Rate', 'USD as Reported' ) then
-                      1 --- Total ARR Filters
-                  when [Type2] in ( 'Total Units' )
-                       and [FX Conversion] in ( 'USD Current Plan Rate' )
-                       and RoutetoMarket2 in ( 'SMB & Consumer', '#' )
-                       and Product not in ( 'Stock D2P Returns', 'Stock on Demand', 'Sign Transactions', 'LIC'
-                                          , 'Shrink', 'CP Default', 'CP005', 'CP016', 'CP040', 'CP080', 'CP150'
-                                          , 'CP500', 'CP001'
-                                          ) then
-                      1
-                  else
-                      0
-              end
+    (
+        (
+            [Fiscal Year] >= '2016'
+            and [Fiscal Year] <= '2018'
+            and [Scenario] = 'Actuals'
+            and [Type2] = 'Total ARR'
+        ) -- Actuals Total ARR from 2016, 2017, 2018
+        or
+        (
+            [Fiscal Year] >= '2017'
+            and [Fiscal Year] <= '2018'
+            and [Scenario] = 'Actuals'
+            and [Type2] = 'Total Units'
+        ) -- Actuals Total Units from 2017, 2018
+        or
+        (
+            [Fiscal Year] >= '2019'
+            and [Fiscal Year] <= '2022'
+            and [Scenario] = ('Actuals - Segmentation')
+        )
+        or
+        (
+            [Fiscal Year] >= '2023'
+            and [Scenario] = ('Actuals')
+        )
+        or
+        (
+            [Fiscal Quarter] = @CQtr
+            and [Scenario] in ( 'Outlook - Current', 'OL - Previous', @Plan, @QRF )
+        ) --- Outlook - Current from Current Fiscal Quarter
+
+        or
+        (
+            [Fiscal Year] > @CY
+            and [Scenario] in ( 'FY24 Plan v1', 'Dashboard QRF' )
+        )
+        or
+        (
+            [Fiscal Year] > @CY
+            and [Scenario] like ('%Ops Plan%')
+        )
+    )
+    and
+    (
+        (
+            Measures3 = 'Beginning'
+            and FiscalWeekNum = 1
+        ) -- Get only data from first week of the quarter
+        or (Measures3 = 'Net New')
+    )
+    and 1 = case
+                when [Type2] in ( 'Total ARR' )
+                     and [FX Conversion] in ( 'USD Current Plan Rate', 'USD as Reported' ) then
+                    1 --- Total ARR Filters
+                when [Type2] in ( 'Total Units' )
+                     and [FX Conversion] in ( 'USD Current Plan Rate' )
+                     and RoutetoMarket2 in ( 'SMB & Consumer', '#' )
+                     and Product not in ( 'Stock D2P Returns', 'Stock on Demand', 'Sign Transactions', 'LIC', 'Shrink'
+                                        , 'CP Default', 'CP005', 'CP016', 'CP040', 'CP080', 'CP150', 'CP500', 'CP001'
+                                        ) then
+                    1
+                else
+                    0
+            end
 --   and [Fiscal Quarter] = '2023 Q2'
 group by [Scenario]
        , Offerings
@@ -251,33 +192,11 @@ group by [Scenario]
        , [RoutetoMarket2]
        , [Fiscal Quarter]
        , case
-             when (Measures3 = 'Beginning')
-                  or
-                  (
-                      Measures3 = 'Net New'
-                      and
-                      (
-                          (
-                              Scenario = 'Actuals - Segmentation'
-                              and FW.[Relative Fiscal Quarter] = @RFQ - 4
-                          )
-                          or
-                          (
-                              Scenario = 'Actuals'
-                              and FW.[Relative Fiscal Quarter] = @RFQ - 1
-                          )
-                          or
-                          -- The below is needed for the R/B/U Ending ARR Adjustment, we need to get the week grain
-                          (
-                              Scenario = 'Actuals'
-                              and [Fiscal Quarter] = '2023 Q1'
-                          )
-                      )
-                      and CAST(right(FW.TM1_Fiscal_WeekinQtr, 2) as int) <= AFW.ActualsFiscalWeek_int
-                  ) then
-                 [Fiscal Week]
+             when FiscalWeekNum < CW.WkNum
+                  and [Route to Market - Subscription] <> 'Enterprise' then
+                 'QTD'
              else
-                 MaxFiscalWeekinYr
+                 'QTG'
          end
        , internal_offering_custom0
        , case
