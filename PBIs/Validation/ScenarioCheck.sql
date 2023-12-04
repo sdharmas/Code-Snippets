128ed6d6661e116d5f9f315ecbdbf9e9b0fe4b5e
sdharmas <102329855+sdharmas@users.noreply.github.com>
2023-12-04 07:51:45 -0800

    deployed variables for Quarter and Scenario

--- PBIs/Validation/ScenarioCheck.sql
+++ PBIs/Validation/ScenarioCheck.sql
@@ -1,4 +1,5 @@
-
+declare @scenario NVARCHAR(30)  = 'Actuals'
+declare @Quarter NVARCHAR(30)   = '2023 Q4'
 
 select [Fiscal Quarter]
      , left(FINSYS_LoadDate, 19)       as LoadDate
@@ -6,8 +7,8 @@ select [Fiscal Quarter]
 from  [FINANCE_SYSTEMS].[dbo].[vw_TM1_Subs_Dash]               
 
 where [Measures3] = 'Net New'
-      and ([Scenario] in ( 'QRF - Mid' ))
-      and [Fiscal Quarter] = '2024 Q3'
+      and [Scenario] = @scenario
+      and [Fiscal Quarter] = @Quarter
       and Type2 = 'Total ARR'
       and [FX Conversion] = 'USD Current Plan Rate'
 group by [Fiscal Quarter]
