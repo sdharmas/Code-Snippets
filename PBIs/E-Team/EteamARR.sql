f43e85e54571a609104dea0f6723d0e218ca4acf
sdharmas <102329855+sdharmas@users.noreply.github.com>
2023-12-19 12:26:14 -0800

    Updated for FY24

--- PBIs/E-Team/EteamARR.sql
+++ PBIs/E-Team/EteamARR.sql
@@ -6,12 +6,10 @@
 -- from [dbo].[Date_FiscalDateLookup] Cal
 -- where DayOfCalendarDate = convert(date, getdate())
 
-declare @CQtr varchar(10)
-declare @CY varchar(10)
+declare @CQtr varchar(10) = '2024 Q1'
+declare @CY varchar(10) = left(@CQtr, 4)
 declare @PQtr varchar(10)
 
-set @CQtr = '2023 Q1'
-set @CY = '2023'
 
 if right(@CQtr, 1) = 1
     -- Get Q4 of prior quarter
@@ -26,36 +24,41 @@ select [FX Conversion]
      , [Ind vs Team vs Ent]
      , [Enterprise BU - Name]
      , [Fiscal Week]
-     , [LoadDate]
+     , [FINSYS_LoadDate]                as LoadDate
      , [Current_FiscalWeek]
      , [Measures2]
      , [Measures3]
      , [RoutetoMarket2]
-     , case
-           when [Route to Market - Subscription] = 'Enterprise' then
-               'ETLA'
-           when [Route to Market - Subscription] in ( 'Phone - Named', 'Reseller - Named' ) then
-               'VIP'
-           when [Route to Market - Subscription] like '%Web%' then
-               'Web'
-           when [Route to Market - Subscription] = 'Phone' then
-               'Phones'
-           when [Route to Market - Subscription] = 'App Store' then
-               'App Store'
-           else
-               'Channel'
-       end                      as RTM2
+     , [Route to Market - Subscription] as RTM3
      , [Type2]
      , [Fiscal Quarter]
      , [Geo]
      , [Current Quarter]
      , [FiscalWeekNum]
      , internal_offering_custom0
-     , sum([Value])             as ValueAdj
+     , sum([Value])                     as ValueAdj
 -- into #ARR
 from [FINANCE_SYSTEMS].[dbo].[vw_TM1_Subs_Dash]
-where [Fiscal Quarter] = @PQtr
-      and [Scenario] = ('Actuals')
+where (
+          /* Current Year Actuals */
+          (
+              [Fiscal Year] = @CY
+              and [Fiscal Quarter] <= @CQtr
+              and [Scenario] = ('Actuals')
+          )
+          /* Prior Year Actuals */
+          or
+          (
+              [Fiscal Year] = @CY - 1
+              and [Fiscal Quarter] <= concat(left(@CQtr, 4) - 1, ' ', right(@CQtr, 2))
+              and [Scenario] = ('Actuals')
+          )
+          /* Current Year Forecast scenarios */
+          or [Fiscal Year] = @CY
+             and [Scenario] in ( '2024 Plan', 'OL - Previous', 'Outlook - Current', 'Q3 QRF 2024', 'Q4 QRF 2024'
+                               , 'Dashboard QRF', 'QRF - Current'
+                               )
+      )
       and
       (
           Measures3 = 'Beginning'
@@ -69,7 +72,7 @@ group by [FX Conversion]
        , [Ind vs Team vs Ent]
        , [Enterprise BU - Name]
        , [Fiscal Week]
-       , [LoadDate]
+       , [FINSYS_LoadDate]
        , [Current_FiscalWeek]
        , [Measures2]
        , [Measures3]
@@ -80,187 +83,7 @@ group by [FX Conversion]
        , [Current Quarter]
        , [FiscalWeekNum]
        , internal_offering_custom0
-       , case
-             when [Route to Market - Subscription] = 'Enterprise' then
-                 'ETLA'
-             when [Route to Market - Subscription] in ( 'Phone - Named', 'Reseller - Named' ) then
-                 'VIP'
-             when [Route to Market - Subscription] like '%Web%' then
-                 'Web'
-             when [Route to Market - Subscription] = 'Phone' then
-                 'Phones'
-             when [Route to Market - Subscription] = 'App Store' then
-                 'App Store'
-             else
-                 'Channel'
-         end
-union all
-
-/*   Prior Quarter */
-select [FX Conversion]
-     , [Scenario]
-     , [Market Segment]
-     , [Ind vs Team vs Ent]
-     , [Enterprise BU - Name]
-     , [Fiscal Week]
-     , [LoadDate]
-     , [Current_FiscalWeek]
-     , [Measures2]
-     , [Measures3]
-     , [RoutetoMarket2]
-     , case
-           when [Route to Market - Subscription] = 'Enterprise' then
-               'ETLA'
-           when [Route to Market - Subscription] in ( 'Phone - Named', 'Reseller - Named' ) then
-               'VIP'
-           when [Route to Market - Subscription] like '%Web%' then
-               'Web'
-           when [Route to Market - Subscription] = 'Phone' then
-               'Phones'
-           when [Route to Market - Subscription] = 'App Store' then
-               'App Store'
-           else
-               'Channel'
-       end                      as RTM2
-     , [Type2]
-     , [Fiscal Quarter]
-     , [Geo]
-     , [Current Quarter]
-     , [FiscalWeekNum]
-     , internal_offering_custom0
-     , sum([Value])             as ValueAdj
--- into #ARR
-from [FINANCE_SYSTEMS].[dbo].[vw_TM1_Subs_Dash]
-where [Fiscal Year] = @CY - 1
-      and [Fiscal Quarter] <= concat(left(@CQtr, 4) - 1, ' ', right(@CQtr, 2))
-      and [Scenario] = ('Actuals - FY22 Segmentation')
-      and
-      (
-          Measures3 = 'Beginning'
-          or Measures3 = 'Net New'
-      )
-      and [FX Conversion] = 'USD Current Plan Rate'
-      and [Type2] = 'Total ARR'
-group by [FX Conversion]
-       , [Scenario]
-       , [Market Segment]
-       , [Ind vs Team vs Ent]
-       , [Enterprise BU - Name]
-       , [Fiscal Week]
-       , [LoadDate]
-       , [Current_FiscalWeek]
-       , [Measures2]
-       , [Measures3]
-       , [RoutetoMarket2]
-       , [Type2]
-       , [Fiscal Quarter]
-       , [Geo]
-       , [Current Quarter]
-       , [FiscalWeekNum]
-       , internal_offering_custom0
-       , case
-             when [Route to Market - Subscription] = 'Enterprise' then
-                 'ETLA'
-             when [Route to Market - Subscription] in ( 'Phone - Named', 'Reseller - Named' ) then
-                 'VIP'
-             when [Route to Market - Subscription] like '%Web%' then
-                 'Web'
-             when [Route to Market - Subscription] = 'Phone' then
-                 'Phones'
-             when [Route to Market - Subscription] = 'App Store' then
-                 'App Store'
-             else
-                 'Channel'
-         end
-union all
-
-/*   Current YEAR */
-select [FX Conversion]
-     , [Scenario]
-     , [Market Segment]
-     , [Ind vs Team vs Ent]
-     , [Enterprise BU - Name]
-     , [Fiscal Week]
-     , [LoadDate]
-     , [Current_FiscalWeek]
-     , [Measures2]
-     , [Measures3]
-     , [RoutetoMarket2]
-     , case
-           when [Route to Market - Subscription] = 'Enterprise' then
-               'ETLA'
-           when [Route to Market - Subscription] in ( 'Phone - Named', 'Reseller - Named' ) then
-               'VIP'
-           when [Route to Market - Subscription] like '%Web%' then
-               'Web'
-           when [Route to Market - Subscription] = 'Phone' then
-               'Phones'
-           when [Route to Market - Subscription] = 'App Store' then
-               'App Store'
-           else
-               'Channel'
-       end                      as RTM2
-     , [Type2]
-     , [Fiscal Quarter]
-     , [Geo]
-     , [Current Quarter]
-     , [FiscalWeekNum]
-     , internal_offering_custom0
-     , sum(   case
-                  when
-                  (
-                      scenario = 'Actuals'
-                      and RoutetoMarket2 = 'All Enterprise'
-                      and [Fiscal Quarter] = @CQtr
-                  ) then
-                      0
-                  else
-                      [Value]
-              end
-          )                     as ValueAdj
--- into #ARR
-from [FINANCE_SYSTEMS].[dbo].[vw_TM1_Subs_Dash]
-where [Fiscal Year] = @CY
-      and [Scenario] in ( '2023 Plan', 'Actuals', 'OL - Previous', 'Outlook - Current' )
-      and
-      (
-          Measures3 = 'Beginning'
-          or Measures3 = 'Net New'
-      )
-      and [FX Conversion] = 'USD Current Plan Rate'
-      and [Type2] = 'Total ARR'
-group by [FX Conversion]
-       , [Scenario]
-       , [Market Segment]
-       , [Ind vs Team vs Ent]
-       , [Enterprise BU - Name]
-       , [Fiscal Week]
-       , [LoadDate]
-       , [Current_FiscalWeek]
-       , [Measures2]
-       , [Measures3]
-       , [RoutetoMarket2]
-       , [Type2]
-       , [Fiscal Quarter]
-       , [Geo]
-       , [Current Quarter]
-       , [FiscalWeekNum]
-       , internal_offering_custom0
-       , case
-             when [Route to Market - Subscription] = 'Enterprise' then
-                 'ETLA'
-             when [Route to Market - Subscription] in ( 'Phone - Named', 'Reseller - Named' ) then
-                 'VIP'
-             when [Route to Market - Subscription] like '%Web%' then
-                 'Web'
-             when [Route to Market - Subscription] = 'Phone' then
-                 'Phones'
-             when [Route to Market - Subscription] = 'App Store' then
-                 'App Store'
-             else
-                 'Channel'
-         end
-
+       , [Route to Market - Subscription]
 
 -- alter table #ARR add Current_FiscalWeek varchar(20)
 
